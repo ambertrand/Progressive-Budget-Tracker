f0a029fb1c2d2befdfd8a46b88538b5f8aa16250
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-10-19 13:29:04 -0400

    setting up express server and mongo

--- server.js
+++ server.js
@@ -3,20 +3,23 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+require('dotenv').config()
 
+// Express
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// connect MongoDB with mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget-Tracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
